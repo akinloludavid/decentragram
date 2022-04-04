65dd6930a7e4303ec47e8499191f30cde206b67f
Akinlolu Ogunleye <akinloludavid2727@gmail.com>
2022-04-04 05:29:53 +0100

    wip

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,40 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    uint256 public imageCount = 0;
+
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event ImageCreated(
+        uint256 id,
+        string hash,
+        string description,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    function uploadImage(string memory _imgHash, string memory _description)
+        public
+    {
+        require
+        imageCount++;
+        images[imageCount] = Image(
+            imageCount,
+            _imgHash,
+            _description,
+            0,
+            msg.sender
+        );
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
+    }
 }
